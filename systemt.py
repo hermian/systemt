8a05e586bf1c809cd18b5407e237b421d548dbd3
sparrowapps <mt.sparrow@gmail.com>
2016-08-02 21:18:18 +0900

    time schedule

--- systemt.py
+++ systemt.py
@@ -2,12 +2,52 @@
 from time import sleep
 from RepeatedTimer import RepeatedTimer
 
-def hello(name):
-    print ("Hello %s!" % name)
+global running
+running = False
 
-print ("starting...")
-rt = RepeatedTimer(1, hello, "World") # it auto-starts, no need of rt.start()
-try:
-    sleep(60*60*24) # your long-running job goes here...
-finally:
-    rt.stop() # better in a try/finally block to make sure the program ends!
+
+def batch(msg):
+    from time import localtime
+    t = localtime()
+
+    global running
+    if running == True: 
+        return
+
+    # 03 reboot
+    if t.tm_hour == 3 and t.tm_min == 0:
+        running = True
+        import cybosplus
+        cybosplus.reboot()
+        running = False
+
+    # 08 cybos plus connect
+    if t.tm_hour == 8 and t.tm_min == 0:
+        running = True
+        import cybosplus
+        cybosplus.run()
+        running = False
+
+    # 16 dataclawler
+    if  t.tm_hour == 16 and t.tm_min == 0:
+        running = True
+        #import cybosplus
+        #cybosplus.run()
+        import datacrawler
+        datacrawler.run()
+        import analyze
+        analyze.run()
+        import backtesting
+        backtesting.run()
+        running = False
+
+# batch process
+if __name__ == '__main__':
+    print ("starting...")
+    rt = RepeatedTimer(1, batch, "batch") # it auto-starts, no need of rt.start()
+    try:
+        sleep(60*60*24) # your long-running job goes here...
+    finally:
+        rt.stop() # better in a try/finally block to make sure the program ends!
+
+   
